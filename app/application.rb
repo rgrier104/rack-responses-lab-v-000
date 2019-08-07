b8af0adb1bcf11937c970fe0273db09235f7e363
Rachel Grier <rgrier104@gmail.com>
2019-08-07 23:12:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,11 @@ class Application
 
       time = Time.new.hour
 
-      resp.write "#{time}"
+      if time < 12
+         resp.write "Good Morning!"
+       else
+         resp.write "Good Afternoon!"
+       end
 
     resp.finish
   end
